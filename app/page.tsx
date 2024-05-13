6fb19b03919680701a6e12c9dc2d0b3515e4244f
tridarma6 <triidarma06@gmail.com>
2024-05-13 16:50:37 +0800

    aodpjlkcna

--- app/page.tsx
+++ app/page.tsx
@@ -45,28 +45,28 @@ export default function Home() {
 						<div className="w-font-rubik font-semibold text-32 text-black"><p className="h-24 w-44">Become a Sponsor</p></div>
 					</div>
 					<p className="mr-20 text-start font-normal tracking-2">As a sponsor, we’re promising reach to thousands of people who are interested in your brand. What better way to get your brand in front of people.</p>
-					<Link href="#" className="mt-4" underline="always">Contact Us<Image className="ml-2 w-3 h-3" src="arrow-right-up.svg" alt="image"/></Link>
+					<Link href="#" className="mt-4" underline="always">Contact Us<Image alt="image" className="ml-2 w-3 h-3" src="arrow-right-up.svg"/></Link>
 				</div>
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-end w-648 left-675 h-323">
-					<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-image.svg" alt="image"/></div>
-      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-182 h-161"><Image src="julias-berger-image.svg" alt="image"/></div>
-      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-image.svg" alt="image"/></div>
-      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-2.svg" alt="image"/></div>
-      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-2.svg" alt="image"/></div>
-      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-regal-blue w-200 h-130"><Image src="add-image.svg" alt="image"/></div>
+					<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image alt="image" src="julias-berger-image.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-182 h-161"><Image alt="image" src="julias-berger-image.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image alt="image" src="julias-berger-image.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image alt="image" src="julias-berger-2.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image alt="image" src="julias-berger-2.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-regal-blue w-200 h-130"><Image alt="image" src="add-image.svg"/></div>
 				</div>
 			</div>
 
 			<div className="mt-44">
 				<div className="flex justify-center ml-[550px]">
-					<Image src="star2.svg" width={40} height={40} className="transform rotate-16.82" alt="image"/>
+					<Image alt="image" src="star2.svg" width={40} height={40} className="transform rotate-16.82"/>
 				</div>
 				<h1 className="font-bold text-center text-regal-blue text-4xl tracking-[0.16em]">SEE OUR COMPETITION </h1>
 			</div>
 			<div className="container mt-28">
 				<div className="w-[1187px] h-[486px] flex justify-between bg-regal-blue border-hidden border-2 border-regal-blue rounded-[20px]">
 					<div className="relative bg-regal-blue w-[593.5px] text-white m-0 rounded-l-[20px]">
-						<Image src="banner-blue.svg" className="z-0" alt="image"/>
+						<Image alt="image" src="banner-blue.svg" className="z-0"/>
 						<div className="absolute inset-0 bottom-0 flex justify-center items-stretch">
 							<p className="text-white text-[48px] font-bold z-10 self-end mx-10 my-10 tracking-[0.1em]">Programming</p>
 						</div>
@@ -77,72 +77,75 @@ export default function Home() {
 						</div>
 						<div className="static flex gap-6">
 							<div className="absolute bottom-0 left-0 mx-10 my-10">
-								<Link href="#" color="foreground" className="text-white text-[32px]">Join <Image src="arrow-right-up.svg" alt="image"/></Link>
+								<Link href="#" color="foreground" className="text-white text-[32px]">Join <Image alt="image" src="arrow-right-up.svg"/></Link>
 							</div>
 							<div className="absolute bottom-0 right-0 mx-10 my-10">
-								<Image src="image.png" alt="image"/>
+								<Image alt="image" src="image.png"/>
 							</div>
 						</div>
 					</div>
 				</div>
 
-				<div className="w-[1187px] h-[486px] flex justify-between bg-regal-blue border-hidden border-2 border-biru-hijau rounded-[20px] mt-16">
-					<div className="bg-biru-hijau w-[593.5px] text-white m-0 rounded-l-[20px]">
-						<Image src="banner-green.svg" className="" alt="image"/>
-					</div>
-					<div className="relative bg-biru-hijau w-[593.5px] text-white m-0 rounded-r-[20px]">
-						<div className="ml-[52.1px] mr-10 mt-[-0.21px] mt-[49.93px]">
-							<p className="font-rubik font-normal text-[20px] text-white flex items-center leading-9">A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions.</p>
-						</div>
-						<div className="static flex gap-6">
-							<div className="absolute bottom-0 left-0 mx-10 my-10">
-								<Link href="#" color="foreground" className="text-white text-[32px]">Join <Image src="arrow-right-up.svg" alt="image"/></Link>
-							</div>
-							<div className="absolute bottom-0 right-0 mx-10 my-10">
-								<Image src="image.png" alt="image"/>
-							</div>
-						</div>
-					</div>
-				</div>
+				<Competition 
+					color="biru-hijau" 
+					srcImage="banner-green.svg" 
+					text="A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions."
+					judul="Web Design"
+				/>
 
-				<div className="w-[1187px] h-[486px] flex justify-between bg-regal-blue border-hidden border-2 border-regal-blue rounded-[20px] mt-16">
-					<div className="bg-regal-blue w-[593.5px] text-white m-0 rounded-l-[20px]">
-						<Image src="banner-blue.svg" className="" alt="image"/>
-					</div>
-					<div className="relative bg-regal-blue w-[593.5px] text-white m-0 rounded-r-[20px]">
-						<div className="ml-[52.1px] mr-10 mt-[-0.21px] mt-[49.93px]">
-							<p className="font-rubik font-normal text-[20px] text-white flex items-center leading-9">A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions.</p>
-						</div>
-						<div className="static flex gap-6">
-							<div className="absolute bottom-0 left-0 mx-10 my-10">
-								<Link href="#" color="foreground" className="text-white text-[32px]">Join <Image src="arrow-right-up.svg" alt="image"/></Link>
-							</div>
-							<div className="absolute bottom-0 right-0 mx-10 my-10">
-								<Image src="image.png" alt="image"/>
-							</div>
-						</div>
-					</div>
-				</div>
+				<Competition 
+					color="regal-blue" 
+					srcImage="banner-blue.svg" 
+					text="A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions."
+					judul="UI UX"
+				/>
 
-				<div className="w-[1187px] h-[486px] flex justify-between bg-regal-blue border-hidden border-2 border-biru-hijau rounded-[20px] mt-16">
-					<div className="bg-biru-hijau w-[593.5px] text-white m-0 rounded-l-[20px]">
-						<Image src="banner-green.svg" className="" alt="image"/>
+				<Competition 
+					color="biru-hijau" 
+					srcImage="banner-green.svg" 
+					text="A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions."
+					judul="KGP"
+				/>
+
+			</div>
+		</>
+	);
+}
+interface CompetitionProps {
+	color: string;
+	srcImage: string;
+	text: string;
+	judul: string;
+}
+
+const Competition: React.FC<CompetitionProps> = ({color, srcImage, text, judul}) => {
+	return(
+		<>
+		<div className={`w-[1187px] h-[486px] flex justify-between bg-${color} border-hidden border-2 border-${srcImage} rounded-[20px] mt-16`}>
+			<div className={`relative bg-${color} w-[593.5px] text-white m-0 rounded-l-[20px]`}>
+				<Image src={srcImage} className="z-0" alt="image"/>
+				<div className="absolute inset-0 bottom-0 flex justify-center items-stretch">
+					<p className="text-white text-[48px] font-bold z-10 self-end mx-10 my-10 tracking-[0.1em]">
+						{judul}
+					</p>
+				</div>
+			</div>
+			<div className={`relative bg-${color} w-[593.5px] text-white m-0 rounded-r-[20px]`}>
+				<div className="ml-[52.1px] mr-10 mt-[-0.21px] mt-[49.93px]">
+					<p className="font-rubik font-normal text-[20px] text-white flex items-center leading-9">
+						{text}
+					</p>
+				</div>
+				<div className="static flex gap-6">
+					<div className="absolute bottom-0 left-0 mx-10 my-10">
+						<Link href="#" color="foreground" className="text-white text-[32px]">Join <Image src="arrow-right-up.svg" alt="image"/></Link>
 					</div>
-					<div className="relative bg-biru-hijau w-[593.5px] text-white m-0 rounded-r-[20px]">
-						<div className="ml-[52.1px] mr-10 mt-[-0.21px] mt-[49.93px]">
-							<p className="font-rubik font-normal text-[20px] text-white flex items-center leading-9">A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions.</p>
-						</div>
-						<div className="static flex gap-6">
-							<div className="absolute bottom-0 left-0 mx-10 my-10">
-								<Link href="#" color="foreground" className="text-white text-[32px]">Join <Image src="arrow-right-up.svg" alt="image"/></Link>
-							</div>
-							<div className="absolute bottom-0 right-0 mx-10 my-10">
-								<Image src="image.png" alt="image"/>
-							</div>
-						</div>
+					<div className="absolute bottom-0 right-0 mx-10 my-10">
+						<Image src="image.png" alt="image"/>
 					</div>
 				</div>
 			</div>
+		</div>
 		</>
 	);
 }
