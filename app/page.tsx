e4cdf82347a82ab6bc4cd6761248c1c98b2d6b3d
PutuRifki <puturifki0@gmail.com>
2024-05-13 09:04:48 +0800

    update all file

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,54 @@ export default function Home() {
 				</div>
 			</div>
 			<Image className="flex w-max mt-12" src="star2.svg" width={40} height={40} alt="gambar"/>
-			<div className="mt-20">
-				<div className="text-center">
-					<h1 className="font-bold text-regal-blue text-4xl font-rubik gap-x-6">WHAT IS ITCC?</h1>
+			<div className="mt-16">
+				<div className="flex flex-row justify-center h-11">
+					<h1 className="font-rubik font-bold text-regal-blue text-4xl font-rubik tracking-widest">WHAT IS ITCC?</h1>
 				</div>
-				<div className="text-center w-1171">
-					<p className="font-normal text-2xl text-balance leading-9">Information Technology Creative Competition (ITCC) is the largest competition in the field of Information Technology in Bali, which is organized by the Information Technology Student Association (HMTI) of Udayana University. From year to year, ITCC always raises interesting topics in the world of technology.</p>
+				<div className="text-center w-1171 h-218 mt-7">
+					<p className="font-rubik font-normal text-2xl leading-normal tracking-4 text-black">Information Technology Creative Competition (ITCC) is the largest competition in the field of Information Technology in Bali, which is organized by the Information Technology Student Association (HMTI) of Udayana University. From year to year, ITCC always raises interesting topics in the world of technology.</p>
+				</div>
+			</div>
+			
+			<div className="flex w-1260 mt-20">
+				<div className="flex flex-col items-start w-489 h-218">
+					<div className="flex flex-col">
+						<div className="w-font-rubik font-semibold text-32 text-black"><p className="h-24 w-44">Become a Sponsor</p></div>
+					</div>
+					<p className="mr-20 text-start font-normal tracking-2">As a sponsor, we’re promising reach to thousands of people who are interested in your brand. What better way to get your brand in front of people.</p>
+					<Link href="#" className="mt-4" underline="always">Contact Us<Image className="ml-2 w-3 h-3" src="arrow-right-up.svg"/></Link>
+				</div>
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-end w-648 left-675 h-323">
+					<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-image.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-182 h-161"><Image src="julias-berger-image.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-image.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-2.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="julias-berger-2.svg"/></div>
+      				<div className="w-full flex justify-center items-center border-solid border-2 rounded-xl border-black w-200 h-130"><Image src="add-image.svg"/></div>
+				</div>
+			</div>
+
+			<div className="mt-44">
+				<div className="flex justify-center ml-[460px]">
+					<Image src="star2.svg" width={40} height={40} className="transform rotate-16.82"/>
+				</div>
+				<h1 className="font-bold text-center text-regal-blue text-4xl">SEE OUR COMPETITION </h1>
+			</div>
+			<div className="container mt-28">
+				<div className="flex w-[1187px] h-[486px] rounded-[20px]">
+					<Image src="banner-programming.svg" className="z-20"/>
+					<div className="bg-regal-blue w-[593.5px] text-white z-10">
+						<p>A community of Influencers and Tech Personalities forging a stage where  the continent’s brightest minds meet new partners, and, the sharpest innovators to share their visions.</p>
+					</div>
+				</div>
+				<div className="w-[1187px] h-[486px]">
+
+				</div>
+				<div className="w-[1187px] h-[486px]">
+
+				</div>
+				<div className="w-[1187px] h-[486px]">
+
 				</div>
 			</div>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,39 @@ module.exports = {
     extend: {
       colors: {
         'regal-blue': '#328AC9',
-        'white': '#FFFFFF'
+        'white': '#FFFFFF',
       },
       width: {
+        '1261': '1261px',
         '1171': '1171px',
-      }
+        '648' : '648px',
+        '489' : '489px',
+        '559' : '559px',
+        '353' : '353px',
+        '200': '200px',
+        '182' : '182px',
+      },
+      height: {
+        '94': '94px',
+        '323': '323px',
+        '218' : '218px',
+        '130' : '130px',
+        '161' : '161px',
+      },
+      fontSize: {
+        '32' : '32px',
+      },
+      letterSpacing: {
+        '2' : '0.02em',
+        '4': '0.04em',
+        widest: '.25em',
+      },
+      rotate: {
+        '16.82': '16.82deg',
+      },
+      marginLeft: {
+        '395' : '395px',
+      },
     },
   },
   darkMode: "class",
